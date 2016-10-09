6bec10f841cde09110d75555e6d180b724d41f71
liyongfu8888 <foever228@163.com>
2016-10-09 21:58:04 +0800

    Add files via upload

---
+++ cyjr-root/src/main/java/cyjr/cyjr_root/App.java
@@ -0,0 +1,13 @@
+package cyjr.cyjr_root;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
